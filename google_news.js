e4f0b605cce6435ceb274c8adf3f2f94f0706eab
crazykeith <shaggysbc69@gmail.com>
2013-07-31 13:19:03 -0400

    Update google_news.js

--- google_news.js
+++ google_news.js
@@ -1,4 +1,7 @@
 jQuery( document ).ready( function($) {
+
+    // Function used to grab the News articles from news.google.com. It uses json and loads the
+    // articles into the results div.
     function getNews( search, num_articles ) {
         if ( search == 'Search Google News' ) {
             search = '';
@@ -15,30 +18,46 @@ jQuery( document ).ready( function($) {
             url: url,
             dataType:'jsonp',
             success: function( json ) {
-                var news_items = json.responseData.feed.entries;
-                var i = 0;
-                $( '.google-search-results' ).html( '' );
-                $( news_items ).each( function () {
-                    // Sorry for this part. The content returned was already formated but
-                    // wasn't playing nice so I had to modify it. I realize this
-                    // is not the best way to format this content, but it was faster than
-                    // reformating it from scratch.
+                if ( json.responseStatus != 200 ) {
+                    var error = '<div class="error">* Sorry a problem occured. Please try again later.</div>'
+                    $( '.google-search-results' ).html( error );
+                } else {
+                    var news_items = json.responseData.feed.entries;
+                    var i = 0;
+                    $( '.google-search-results' ).html( '' );
+                    $( news_items ).each( function () {
+                        // Sorry for this part. The content returned was already formatted but
+                        // wasn't playing nice so I had to modify it. I realize this
+                        // is not the best way to format this content, but it was faster than
+                        // reformatting it from scratch.
 
-                    var bad_div = 'font-size:85%;font-family:arial,sans-serif"><br><div ' +
-                                'style="padding-top:0.8em"><img alt="" height="1" width="1"></div>';
-                    var new_content = news_items[i].content
-                                    .replace(bad_div,'font-size:120%;font-family:arial,sans-serif">');
-                    $( '.google-search-results' ).append( new_content );
-                    i++;
-                })
+                        var bad_div = 'font-size:85%;font-family:arial,sans-serif"><br><div ' +
+                                    'style="padding-top:0.8em"><img alt="" height="1" width="1"></div>';
+                        var new_content = news_items[i].content
+                                        .replace(bad_div,'font-size:120%;font-family:arial,sans-serif">');
+                        $( '.google-search-results' ).append( new_content );
+                        i++;
+                    })
+                }
+            },
+            error: function() {
+                var error = '<div class="error">* Sorry a problem occured. Please try again later.</div>'
+                $( '.google-search-results' ).html( error );
             }
         });
     }
+
+    // This is to be called when the page loads. It tests if the search text box has the
+    // default text in it, if not then it calls the getNews function on the text in the box.
+    // This is used for the optional shortcode parameters.
     if ( $( ".google-search-text" ).val() != "Search Google News" ) {
         var search = $( '.google-search-text' ).val();
         var num_articles = $( '.google-search-num-articles' ).val();
         getNews( search, num_articles );
     }
+
+    // This function is to gather the search data when the search button is
+    // pressed and call the getNews function.
     $( ".google-search-button" ).click( function(event) {
         event.preventDefault();
         $( '.google-search-results' ).html( '' );
@@ -47,11 +66,15 @@ jQuery( document ).ready( function($) {
         getNews( search, num_articles );
     })
 
+    // This is to empty the search text box if its clicked and has the default statement.
     $( ".google-search-text" ).focus(function() {
         if ( $( this ).val() == 'Search Google News' ) {
             $( this ).val( '' );
         }
     })
+
+    // This is to reassign the default statement into the search text box if the
+    // box is empty when the user clicks or tabs away.
     $( ".google-search-text" ).blur( function() {
         if ( $( this ).val() == '' ) {
             $( this ).val( 'Search Google News' );
@@ -61,12 +84,12 @@ jQuery( document ).ready( function($) {
     $( "input.google-search-text" ).bind( "keydown", function(event) {
         // track enter key
         var keycode = ( event.keyCode ? event.keyCode : ( event.which ? event.which : event.charCode));
-        if ( keycode == 13 ) { // keycode for enter key
+        if ( keycode == 13 ) { // key code for enter key
             // force the 'Enter Key' to implicitly click the Add items button
             $( '.google-search-button' ).click();
             return false;
         } else  {
             return true;
         }
-    }); // end of function
-})
+    });
+});
